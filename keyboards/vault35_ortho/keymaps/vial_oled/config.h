2984c74ca260ab2a13dc38700813bb7be8244eaf
Daniel Ma <dma18@berkeley.edu>
2022-08-27 15:47:11 -0400

    Add vault ortho vial qmk firmware

---
+++ keyboards/vault35_ortho/keymaps/vial_oled/config.h
@@ -0,0 +1,14 @@
+#define VIAL_KEYBOARD_UID {0xC9, 0xFC, 0x26, 0x3D, 0xDF, 0xF4, 0x86, 0xFA}
+
+// // ENCODER 
+// #define ENCODERS_PAD_A { B5, D1 }
+// #define ENCODERS_PAD_B { B6, D0 }
+// #define ENCODER_DIRECTION_FLIP
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
+
+#define OLED_TIMEOUT 120000
+#define OLED_FADE_OUT true
+#define OLED_FADE_OUT_INTERVAL 0
+
